94f00080339d83c84fab7b3d7f268543734ce18f
xasan <xasan2006@gmail.com>
2020-11-20 14:05:46 +0500

    first upload

---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,21 @@
+﻿using Foundation;
+using UIKit;
+
+namespace BlinkIDFormsSample
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+            Xamarin.Forms.DependencyService.Register<Microblink.Forms.iOS.MicroblinkScannerFactoryImplementation>();
+
+			LoadApplication (new BlinkIDApp.App());
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
